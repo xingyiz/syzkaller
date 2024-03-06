28eb26d742349aafe36ce4153e9deb080ca2942b
Han Xing Yi <hxingyi104@gmail.com>
2024-03-06 21:33:39 +0800

    don't pin thread to cpu

--- executor/executor.cc
+++ executor/executor.cc
@@ -1242,34 +1242,34 @@ void thread_mmap_cover(thread_t* th)
 	cover_protect(&th->cov);
 }
 
-int stick_this_thread_to_core(int core_id)
-{
-	int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
-	if (core_id < 0 || core_id >= num_cores)
-		return EINVAL;
+// int stick_this_thread_to_core(int core_id)
+// {
+// 	int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+// 	if (core_id < 0 || core_id >= num_cores)
+// 		return EINVAL;
 
-	cpu_set_t cpuset;
-	CPU_ZERO(&cpuset);
-	CPU_SET(core_id, &cpuset);
+// 	cpu_set_t cpuset;
+// 	CPU_ZERO(&cpuset);
+// 	CPU_SET(core_id, &cpuset);
 
-	pthread_t current_thread = pthread_self();
-	return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
-}
+// 	pthread_t current_thread = pthread_self();
+// 	return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
+// }
 
 void* worker_thread(void* arg)
 {
-	thread_t* th = (thread_t*)arg;
-	current_thread = th;
+	// thread_t* th = (thread_t*)arg;
+	// current_thread = th;
 
-	int id = th->id;
-	int core_id = id % sysconf(_SC_NPROCESSORS_ONLN);
-	if (stick_this_thread_to_core(core_id) != 0)
-		fail("stick_this_thread_to_core failed");
+	// int id = th->id;
+	// int core_id = id % sysconf(_SC_NPROCESSORS_ONLN);
+	// if (stick_this_thread_to_core(core_id) != 0)
+	// 	fail("stick_this_thread_to_core failed");
 
 	pid_t tid = syscall(SYS_gettid);
 	int policy = sched_getscheduler(tid);
-	int cpu = sched_getcpu();
-	fprintf(stderr, "[XY_LOG] thread %d: start (policy=%d cpu=%d)\n", tid, policy, cpu);
+	// int cpu = sched_getcpu();
+	fprintf(stderr, "[XY_LOG] thread %d: start (policy=%d)\n", tid, policy);
 
 	if (cover_collection_required())
 		cover_enable(&th->cov, flag_comparisons, false);
